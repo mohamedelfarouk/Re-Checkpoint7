efb6183a208f38d275ae82cd7e6bc45ac4f72817
mohamedelfarouk <mohameda.elfarouk@gmail.com>
2023-03-16 17:00:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [person, setPerson] = useState({
+    fullname: "Mohamed Amr Omar El-Farouk",
+    bio: "Lorem ipsum dolor sit amet.",
+    imgSrc: "./1.jpg",
+    profession: "Student",
+    shows: true,
+  });
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    if (person.shows)
+      setPerson({
+        fullName: "",
+        bio: "",
+        imgSrc: "",
+        profession: "",
+        shows: false,
+      });
+    else
+      setPerson({
+        fullname: "Mohamed Amr Omar El-Farouk",
+        bio: "Lorem ipsum dolor sit amet.",
+        imgSrc: "./1.jpg",
+        profession: "Student",
+        shows: true,
+      });
+    setCount(0);
+  };
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <h1>{person.fullname}</h1>
+      <h2>{person.bio}</h2>
+      <h3>{person.profession}</h3>
+      <img src={person.imgSrc} />
+      <br />
+      <button style={{ width: "4rem", height: "4rem" }} onClick={handleClick}>
+        {person.shows ? "Hide" : "Show"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
